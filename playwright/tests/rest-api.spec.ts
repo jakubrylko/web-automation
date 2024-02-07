a666a5a7c72384eab8dd952d8fba26c9c5aaafa0
Jakub Ryłko <jrylko@apptension.com>
2024-02-07 20:53:55 +0100

    Fix API url

--- playwright/tests/rest-api.spec.ts
+++ playwright/tests/rest-api.spec.ts
@@ -1,18 +1,19 @@
 import { expect, test } from '@playwright/test'
 import { NEW_BOOKING_BODY } from '@common/api/body'
 
-const BASE_URL = 'https://restful-booker.herokuapp.com'
-
 test.describe('Restful Booker API', () => {
   test('Should get bookings list', async ({ request }) => {
-    const response = await request.get(`${BASE_URL}/booking`)
+    const response = await request.get(
+      'https://restful-booker.herokuapp.com/booking'
+    )
     expect(response.status()).toEqual(200)
   })
 
   test('Should create new booking', async ({ request }) => {
-    const response = await request.post(`${BASE_URL}/booking`, {
-      data: NEW_BOOKING_BODY,
-    })
+    const response = await request.post(
+      'https://restful-booker.herokuapp.com/booking',
+      { data: NEW_BOOKING_BODY }
+    )
     expect(response.status()).toEqual(200)
     const responseJSON = await response.json()
     console.log(`Booking id: ${responseJSON.bookingid}`)
