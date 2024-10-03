541fbf658148ae9978e36693e064ef1c45feb3e6
Jakub Ryłko <jrylko@apptension.com>
2024-10-03 09:17:54 +0200

    Update pw config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig, devices } from '@playwright/test'
-
-require('dotenv').config()
+import 'dotenv/config'
 
 export default defineConfig({
   testDir: './playwright/tests',
   outputDir: './playwright/test-results',
   snapshotDir: './playwright/snapshots',
-  expect: { toHaveScreenshot: { maxDiffPixels: 100 } },
+
+  timeout: 30000,
+  expect: { timeout: 5000, toHaveScreenshot: { maxDiffPixels: 100 } },
+
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
+
   reporter: [
     ['html', { open: 'never', outputFolder: './playwright/playwright-report' }]
   ],
