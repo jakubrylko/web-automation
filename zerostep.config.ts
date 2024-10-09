bd0b874b4d4ca168e69040f02151c797faa559e8
Jakub Ryłko <jrylko@apptension.com>
2024-10-09 20:02:40 +0200

    Update zerostep config

--- zerostep.config.ts
+++ zerostep.config.ts
@@ -1,8 +1,8 @@
 import 'dotenv/config'
 
-// App: https://app.zerostep.com
-// Docs: https://github.com/zerostep-ai/zerostep
-
 export default {
   TOKEN: process.env.TOKEN
 }
+
+// App: https://app.zerostep.com
+// Docs: https://github.com/zerostep-ai/zerostep
