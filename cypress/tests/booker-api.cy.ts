0c2fd39f85b37552431a95d808d9997b7a9d87ed
Jakub Ryłko <jakub.rylko@gmail.com>
2024-10-11 16:27:49 +0200

    Update cypress tests

--- cypress/tests/booker-api.cy.ts
+++ cypress/tests/booker-api.cy.ts
@@ -1,18 +1,18 @@
 import { newBooking } from 'common'
 import { statusShouldBeOk } from 'cypress/api/utilities'
-import * as Booker from 'cypress/api/Booker'
+import * as BookerAPI from 'cypress/api/Booker'
 
 describe('Restful Booker API', () => {
   it('Should get bookings list', () => {
-    Booker.getBookings().then((response) => {
+    BookerAPI.getBookings().then((response) => {
       statusShouldBeOk(response)
     })
   })
 
   it('Should create new booking', () => {
-    Booker.createBooking(newBooking).then((response) => {
+    BookerAPI.createBooking(newBooking).then((response) => {
       statusShouldBeOk(response)
-      Booker.assertCreatedBooking(response)
+      BookerAPI.assertCreatedBooking(response)
     })
   })
 })
