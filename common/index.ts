376fd4c02f93465edf6e390183ba7f74f63f7a99
Jakub Ryłko <91373813+jakubrylko@users.noreply.github.com>
2024-10-17 14:42:36 +0200

    Add cypress workflow (#18)
    * Add cypress workflow
    
    * Fix cypress workflow
    
    * Fix cypress workflow
    
    * Fix cypress workflow
    
    * Fix cypress workflow
    
    * Update cypress workflow
    
    * Update cypress workflow
    
    * Update cypress workflow
    
    * Update cypress workflow
    
    * Update cypress workflow
    
    * Update cypress workflow
    
    * Update cypress workflow
    
    * Update cypress workflow
    
    * Update cypress config

--- common/index.ts
+++ common/index.ts
@@ -1,3 +1,4 @@
 export * from './booking'
 export * from './card-details'
-export * from './windows'
+export * from './labels'
+export * from './viewports'
--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,9 @@
 import { allureCypress } from 'allure-cypress/reporter'
 import { defineConfig } from 'cypress'
+import { viewport } from './common'
 import 'dotenv/config'
+
+const { CI, DEVICE } = process.env
 
 export default defineConfig({
   e2e: {
@@ -8,17 +11,19 @@ export default defineConfig({
       allureCypress(on, config, {
         resultsDir: 'allure/allure-results/cypress'
       })
+
       Object.assign(config.env, process.env)
       return config
     },
-    baseUrl: 'https://demoqa.com',
 
+    baseUrl: 'https://demoqa.com',
+    video: CI ? true : false,
     chromeWebSecurity: false,
     defaultCommandTimeout: 4000,
     pageLoadTimeout: 60000,
-    retries: { runMode: 0, openMode: 0 },
-    viewportHeight: 1080,
-    viewportWidth: 1920,
+    retries: { runMode: CI ? 1 : 0, openMode: 0 },
+    viewportHeight: viewport[DEVICE ?? 'MacBook'].height,
+    viewportWidth: viewport[DEVICE ?? 'MacBook'].width,
     watchForFileChanges: false,
 
     specPattern: 'cypress/tests/**/*.cy.{js,ts}',
