c2f0b471ca448702ca0273c711692aec2a2c96cc
Jakub Ryłko <jakub.rylko@gmail.com>
2024-11-06 10:08:05 +0100

    Implement file upload

--- playwright/shared/Utilities.ts
+++ playwright/shared/Utilities.ts
@@ -1,5 +1,4 @@
 import { Locator, Page, expect } from '@playwright/test'
-
 export class Utilities {
   constructor(private page: Page) {
     this.page = page
--- playwright/tests/demoqa/form.spec.ts
+++ playwright/tests/demoqa/form.spec.ts
@@ -26,7 +26,7 @@ test.describe('Practice form', () => {
     await PracticeForm.assertSubject('Computer Science')
     await PracticeForm.selectHobbies(['Sports', 'Music'])
 
-    const filePath = path.resolve(__dirname, 'common/assets/qa.jpg')
+    const filePath = path.resolve(process.cwd(), '../common/assets/qa.jpg')
     await PracticeForm.chooseFileButton.setInputFiles(filePath)
 
     await PracticeForm.currentAddress.fill('Gorecka 1,\nPoznan,\nPoland')
