e9511786ee5c2bd171d1aa8da194857ef3b923a8
Jakub Ryłko <jakub.rylko@gmail.com>
2024-11-22 18:03:47 +0100

    Implement graphql mocking tests

--- playwright/pages/Boilerplate/Homepage/Home.selectors.ts
+++ playwright/pages/Boilerplate/Homepage/Home.selectors.ts
@@ -3,18 +3,18 @@ import { BaseClass } from 'playwright/shared/BaseClass'
 
 export class HomeSelectors extends BaseClass {
   readonly bellWithDot: Locator
-  readonly notificationBell: Locator
   readonly notificationList: Locator
   readonly notification: Locator
+  readonly notificationBell: Locator
   readonly toast: Locator
 
   constructor(page: Page) {
     super(page)
 
     this.bellWithDot = page.locator('.lucide-bell-dot')
-    this.notificationBell = page.getByTestId('notifications-trigger-testid')
     this.notificationList = page.getByRole('dialog')
     this.notification = this.notificationList.locator('li')
+    this.notificationBell = page.getByTestId('notifications-trigger-testid')
     this.toast = page.getByTestId('toast-1')
   }
 }
